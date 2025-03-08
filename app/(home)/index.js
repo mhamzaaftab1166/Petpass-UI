082e182ba0798b193ce4ebdd562b3a16390d0629
Syed Suhaib Zia <suhaibzia786@gmail.com>
2025-03-08 15:47:07 +0400

    Home Screen Title and Recent Card and Tips card added

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -14,22 +14,30 @@ import Banner from "../components/HomePage/Banner";
 import Categories from "../components/HomePage/Categories";
 import { Stack } from "expo-router";
 import PetBanner from "../components/HomePage/PetBanner";
+import RecentPets from "../components/HomePage/recentpets";
+import PetTips from "../components/HomePage/PetTips";
+import { useTheme } from "../helper/themeProvider";
 
 const { width, height } = Dimensions.get("screen");
 
 const Index = () => {
+    const { isDarkMode } = useTheme();
+  console.log(isDarkMode, 'isDarkMode');
+  
   return (
     <>
       <ScrollView
         showsVerticalScrollIndicator={false}
-        style={{ marginTop: 20 }}
+        style={{ marginTop: 20, backgroundColor: isDarkMode ? Colors.dark : Colors.secondary }}
         contentContainerStyle={{
-          height: Platform.OS === "ios" ? height * 1.25 : height * 1.15,
+          height: Platform.OS === "ios" ? height * 1.25 : height * 2,
         }}
       >
       <Banner />
        <Categories/>
        <PetBanner />
+       <RecentPets />
+       <PetTips />
       </ScrollView>
     </>
   );
