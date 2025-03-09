ae7f4ea88c8db37d7b15791e7980d5e9855b512a
hamzadev <hamzaaftab.dev@gmail.com>
2025-03-09 13:36:03 +0400

    refactored

--- app/(home)/index.js
+++ app/(home)/index.js
@@ -1,10 +1,5 @@
-
 import React from "react";
-import {
-  Dimensions,
-  ScrollView,
-  Platform,
-} from "react-native";
+import { Dimensions, ScrollView, Platform } from "react-native";
 import Banner from "../components/HomePage/Banner";
 import Categories from "../components/HomePage/Categories";
 import PetBanner from "../components/HomePage/PetBanner";
@@ -13,28 +8,31 @@ import PetTips from "../components/HomePage/PetTips";
 import { useTheme } from "../helper/themeProvider";
 import { Colors } from "../theme/color";
 import "react-native-get-random-values";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const { width, height } = Dimensions.get("screen");
 
 const Index = () => {
-    const { isDarkMode } = useTheme();
-  console.log(isDarkMode, 'isDarkMode');
-  
+  const { isDarkMode } = useTheme();
+  console.log(isDarkMode, "isDarkMode");
+
   return (
     <>
       <ScrollView
         showsVerticalScrollIndicator={false}
-        style={{ marginTop: 20, backgroundColor: isDarkMode ? Colors.dark : Colors.secondary }}
+        style={{
+          marginTop: 20,
+          backgroundColor: isDarkMode ? Colors.dark : Colors.secondary,
+        }}
         contentContainerStyle={{
           height: Platform.OS === "ios" ? height * 1.25 : height * 2,
         }}
       >
-      <Banner />
-       <Categories/>
-       <PetBanner />
-       <RecentPets />
-       <PetTips />
+        <Banner />
+        <Categories />
+        <PetBanner />
+        <RecentPets />
+        <PetTips />
       </ScrollView>
     </>
   );
